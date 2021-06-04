47c9a2eb06db276c450a067a08fcf27c8790c4cd
Winstons11 <Vitalik.boy009@gmail.com>
2021-06-04 18:54:31 +0300

    11

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-document.querySelector('.services__box').addEventListener('click', () => {
-   document.querySelector('.rectangle__box').classList.toggle('active');
+document.querySelector('.__1').addEventListener('click', () => {
+   document.querySelector('.__11').classList.toggle('active');
+});
+document.querySelector('.__2').addEventListener('click', () => {
+   document.querySelector('.__22').classList.toggle('active');
+});
+document.querySelector('.__3').addEventListener('click', () => {
+   document.querySelector('.__33').classList.toggle('active');
+});
+document.querySelector('.__4').addEventListener('click', () => {
+   document.querySelector('.__44').classList.toggle('active');
+});
+document.querySelector('.__5').addEventListener('click', () => {
+   document.querySelector('.__55').classList.toggle('active');
 });
