5f81e64333d4cc596e04b1f381e73300a9303950
Winstons11 <Vitalik.boy009@gmail.com>
2021-06-05 12:09:13 +0300

    11

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
-document.querySelector('.__1').addEventListener('click', () => {
-   document.querySelector('.__11').classList.toggle('active');
-});
-document.querySelector('.__2').addEventListener('click', () => {
-   document.querySelector('.__22').classList.toggle('active');
-});
-document.querySelector('.__3').addEventListener('click', () => {
-   document.querySelector('.__33').classList.toggle('active');
-});
-document.querySelector('.__4').addEventListener('click', () => {
-   document.querySelector('.__44').classList.toggle('active');
-});
-document.querySelector('.__5').addEventListener('click', () => {
-   document.querySelector('.__55').classList.toggle('active');
-});
+const accordions = document.querySelectorAll('.services__box');
+
+for(item of accordions) {
+   item.addEventListener('click', function() {
+      this.classList.toggle('active');
+   })
+}
