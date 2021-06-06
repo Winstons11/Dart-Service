c76fba6a241598918732b5dff49d449355b5ab32
Winstons11 <Vitalik.boy009@gmail.com>
2021-06-06 13:40:43 +0300

    1

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ const accordions = document.querySelectorAll('.services__box');
 for(item of accordions) {
    item.addEventListener('click', function() {
       this.classList.toggle('active');
-   })
-}
+   });
+};
+
